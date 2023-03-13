b09b37fecd6c1d04df8df33d8361f508dfd834dc
Luiz Gustavo Banin de Sena <luizgbs1@gmail.com>
2023-03-13 18:19:28 -0300

    create a project home

---
+++ home/src/main/bootstrap.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import "./index.css";
+
+const App = () => (
+  <div className="container">
+    <div>Name: home</div>
+    <div>Framework: react</div>
+    <div>Language: TypeScript</div>
+    <div>CSS: Empty CSS</div>
+  </div>
+);
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById("main")
+);
---
+++ home/webpack.config.js
@@ -0,0 +1,77 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+
+const deps = require("./package.json").dependencies;
+const path = require('path');
+module.exports = {
+  mode: 'development',
+	entry: './src/main/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'public/js'),
+    filename: 'bundle.js'
+  },
+
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+
+  devServer: {
+    port: 3001,
+    historyApiFallback: true,
+    devMiddleware: {
+      writeToDisk: true
+    },
+    static: {
+      directory: './public'
+    },
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "home",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {},
+      shared: {
+        ...deps,
+        react: {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps.react,
+        },
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
